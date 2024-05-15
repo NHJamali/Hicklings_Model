212a81bc950822573b3c108159b49297e75af0d2
Noor Hassan Jamali <nhjamali@outlook.com>
2024-05-15 09:54:08 +0200

    Basic_changes

--- Hicklings_Lung_Model.m
+++ Hicklings_Lung_Model.m
@@ -1,10 +1,8 @@
-function [pressure, volume] =Hicklings_Lung_Model(press_range,TOP)
-ALV=0.0002;
-REM=27000*ALV;
+function [pressure, volume] =Hicklings_Lung_Model(press_range,opmin,opmax)
+%ALV=0.0002;
+%REM=27000*ALV;
 PIP=press_range(2);
 PEEP=press_range(1);
-opmin=TOP(1);
-opmax=TOP(2);
 volume=[];
 pressure=[];
 for press=PEEP:PIP
@@ -13,10 +11,10 @@ for press=PEEP:PIP
         for opress=opmin:opmax
             if press> (sp+opress)
                 vol=(press - sp)*9000*0.0002/(1+opmax-opmin);
-                vol=2*vol-0.12*vol^1.7;
+                %vol=2*vol-0.12*vol^1.7;
             elseif PIP > (sp+opress) && PEEP > sp
                 vol=(press-sp)*9000*0.0002/(1+opmax-opmin);
-                vol=2*vol-0.12*vol^1.7;
+                %vol=2*vol-0.12*vol^1.7;
             else
                 vol=0;
             end
--- hicklingmodel.m
+++ hicklingmodel.m
@@ -3,14 +3,16 @@ PEEP= Pressure_range(1);
 PIP= Pressure_range(2);
 volume=[];
 pressure=[];
-Pressure_range= [PEEP, PIP];
+%Pressure_range= [PEEP, PIP];
 for PRESS=PEEP:PIP 
 VOLTOT=0; 
   for SP= -0.5:0.5:14.5
     if PRESS>(SP+TOP)
-      VOL=(PRESS-SP)*9000*0.0002; 
+      VOL=(PRESS-SP)*9000*0.0002;
+
     elseif PIP>(SP+TOP)&&PEEP>SP 
-      VOL=(PRESS-SP)*9000*0.0002; 
+      VOL=(PRESS-SP)*9000*0.0002;
+      
     else
       VOL=0;
     end
