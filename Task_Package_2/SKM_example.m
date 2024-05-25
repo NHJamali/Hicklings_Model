6b59a452f080b10623c2f28dcfa9b660818782ed
Noor Hassan Jamali <nhjamali@outlook.com>
2024-05-25 15:19:37 +0200

    Update SKM_example.m

--- Task_Package_2/SKM_example.m
+++ Task_Package_2/SKM_example.m
@@ -12,9 +12,13 @@ C=zeros(1,27);
 for i=0:26
     C(i+1)=(V(i+2)-V(i+1))/(P(i+2)-P(i+1));
 end
-hold on
 subplot(2,1,2)
 plot(P(2:end),C)
 xlabel("Pressure (Cm H2O)")
 ylabel("Compliance")
 title("Change in Compliance-SKM")
+
+
+hold on
+C_skm=C_SKM(P,0.365,0.0977);
+plot(P,C_skm)
