1f7bf866a2d394615c486c3c85f29e7327ebe4ce
Noor Hassan Jamali <nhjamali@outlook.com>
2024-07-11 19:10:26 +0200

    Update Task_c_layers.m

--- Files for submission/Package_1/Task_c_layers.m
+++ Files for submission/Package_1/Task_c_layers.m
@@ -1,19 +1,18 @@
 PEEP=0;
 PIP=35;
-TOP=[0 6 12 15];
-layers=30;
+TOP=0;
+layers=[4 10 20 30];
 CompPerUnit=0.0002;
 UnitsPerLayers=9000;
 figure
-for i=1:length(TOP)
-    [volume,pressure]=BasicHickling(PEEP,PIP,TOP(i),layers,CompPerUnit,UnitsPerLayers);
+for i=1:length(layers)
+    [volume,pressure]=BasicHickling(PEEP,PIP,TOP,layers(i),CompPerUnit,UnitsPerLayers);
     plot(pressure,volume)
     grid on
     hold on
 end
 
 
-title('Hickling Model (Different TOP)')
-legend("TOP=0","TOP=6","TOP=12","TOP=15")
+title('Hickling Model , Different Number of Layers TOP=0')
 xlabel("Pressure")
 ylabel("Volume")
